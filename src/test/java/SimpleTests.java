642298b1638514c5167a69e4179bac269c56e884
DanVu <danvu@ya.ru>
2021-02-18 10:34:38 +0300

    added simple tests

--- src/test/java/SimpleTests.java
+++ src/test/java/SimpleTests.java
@@ -6,12 +6,11 @@ import static org.junit.jupiter.api.Assertions.*;
 public class SimpleTests {
     @Test
     void successTest() {
-        assertEquals(true, true);
+        assertTrue(true);
     }
-
 
     @Test
     void negativeTest() {
-        assertEquals(true, false);
+        assertTrue(false);
     }
 }
