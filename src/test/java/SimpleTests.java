3bd48f742993357ec088e9d05e6f7968c0f1f3f4
DanVu <danvu@ya.ru>
2021-02-18 16:34:20 +0300

    added allure

--- src/test/java/SimpleTests.java
+++ src/test/java/SimpleTests.java
@@ -1,16 +1,30 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class SimpleTests {
     @Test
     void successTest() {
+
         assertTrue(true);
     }
 
     @Test
     void negativeTest() {
-        assertTrue(false);
+        Assertions.fail()
+    }
+
+    @Test
+    void successWithHamcrestTest() {
+        assertThat(true, is(true));
+    }
+
+    @Test
+    void negativeWithHamcrestTest() {
+        assertThat(true, is(false));
     }
 }
