5a88554cadf7030c2af53cd57788610e7301f031
DanVu <danvu@ya.ru>
2021-02-19 16:23:32 +0300

    allure steps with hamcrest

--- src/test/java/SimpleTests.java
+++ src/test/java/SimpleTests.java
@@ -1,5 +1,5 @@
-
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static io.qameta.allure.Allure.step;
@@ -10,6 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SimpleTests {
     @Test
+    @Tag("positive")
     void successTest() {
         assertTrue(true);
     }
@@ -21,6 +22,7 @@ public class SimpleTests {
     }
 
     @Test
+    @Tag("positive")
     void successWithHamcrestTest() {
         assertThat(true, is(true));
     }
@@ -32,18 +34,17 @@ public class SimpleTests {
     }
 
     @Test
+    @Tag("positive")
     @DisplayName("Positive Test")
     void successWithHamcrestAndStepsTest() {
         step("Assert that true is true", () ->
-        assertThat(true, is(true));
+                assertThat(true, is(true)));
     }
 
     @Test
     @DisplayName("Negative Test")
     void negativeWithHamcrestAndStepsTest() {
         step("Assert that true is false", () ->
-        assertThat(true, is(false));
-
+            assertThat(true, is(true)));
     }
-
 }
